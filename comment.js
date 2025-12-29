67593a70ead627de28121b7c439bfa4b42a21dd2
2024tonegonproject <tgrk_2024tgProject_Official@outlook.jp>
2025-12-29 16:53:52 +0900

    upd

--- comment.js
+++
@@ -1,8 +0,0 @@
-fetch("https://noisy-sound-60af.tgrk-2024tgproject-official.workers.dev/", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    name: "nanasi",
-    message: "thisisantestcode."
-  })
-})
