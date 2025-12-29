914bdc759a01c564cd768bdcea5ea32e7c50e065
2024tonegonproject <tgrk_2024tgProject_Official@outlook.jp>
2025-12-29 14:46:39 +0900

    Update fade.js

--- fade.js
+++ fade.js
@@ -3,21 +3,18 @@ fetch("projects.json?v=" + Date.now())
   .then(projects => {
     const container = document.getElementById("scratch-projects");
 
-    projects.forEach(project => {
+    projects.forEach((project, index) => {
       const link = document.createElement("a");
-      link.href = `https://scratch.mit.edu/projects/${project.id}/`;
+      link.href = `https://scratch.mit.edu/projects/${project}/`;
       link.target = "_blank";
 
-      link.innerHTML = `
-        <img 
-          src="https://cdn2.scratch.mit.edu/get_image/project/${project.id}_282x210.png"
-          class="scratch-thumb"
-        >
-      `;
+      const img = document.createElement("img");
+      img.src = `https://cdn2.scratch.mit.edu/get_image/project/${project}_282x210.png`;
+      img.classList.add("scratch-thumb");
+
       img.style.animationDelay = `${index * 0.1}s`;
 
       link.appendChild(img);
-
       container.appendChild(link);
     });
   });
