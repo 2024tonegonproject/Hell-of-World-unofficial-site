e255f360add5b9592ef10eec4a69ca37612402b8
2024tonegonproject <tgrk_2024tgProject_Official@outlook.jp>
2025-12-29 14:44:30 +0900

    upd

--- fade.js
+++ fade.js
@@ -1,15 +1,23 @@
-projects.forEach((project, index) => {
-  const link = document.createElement("a");
-  link.href = `https://scratch.mit.edu/projects/${project}/`;
-  link.target = "_blank";
+fetch("projects.json?v=" + Date.now())
+  .then(res => res.json())
+  .then(projects => {
+    const container = document.getElementById("scratch-projects");
 
-  const img = document.createElement("img");
-  img.src = `https://cdn2.scratch.mit.edu/get_image/project/${project}_282x210.png`;
-  img.classList.add("scratch-thumb");
+    projects.forEach(project => {
+      const link = document.createElement("a");
+      link.href = `https://scratch.mit.edu/projects/${project.id}/`;
+      link.target = "_blank";
 
-  // ここで遅延を追加
-  img.style.animationDelay = `${index * 0.1}s`;
+      link.innerHTML = `
+        <img 
+          src="https://cdn2.scratch.mit.edu/get_image/project/${project.id}_282x210.png"
+          class="scratch-thumb"
+        >
+      `;
+      img.style.animationDelay = `${index * 0.1}s`;
 
-  link.appendChild(img);
-  container.appendChild(link);
-});
+      link.appendChild(img);
+
+      container.appendChild(link);
+    });
+  });
