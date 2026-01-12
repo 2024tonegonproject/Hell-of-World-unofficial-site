37d8227f81f4b71e5144567060d5d5383deac189
2024tonegonproject <tgrk_2024tgProject_Official@outlook.jp>
2026-01-12 11:48:03 +0900

    Update fade.js

--- fade.js
+++ fade.js
@@ -1,22 +1,19 @@
 fetch("https://2024tonegonproject.github.io/Hell-of-World-official-site/projects.json?v=" + Date.now())
   .then(res => res.json())
-
   .then(projects => {
     const container = document.getElementById("scratch-projects");
 
     projects.forEach((item, index) => {
-
       const id = item.id;
-      const title = item.title;
-      const image = item.image;
-      const username = item.username;
+
+      const thumb = `https://cdn2.scratch.mit.edu/get_image/project/${id}_282x210.png`;
 
       const link = document.createElement("a");
       link.href = `https://scratch.mit.edu/projects/${id}/`;
       link.target = "_blank";
 
       const img = document.createElement("img");
-      img.src = image;
+      img.src = thumb;
       img.classList.add("scratch-thumb");
 
       img.style.animationDelay = `${index * 0.1}s`;
